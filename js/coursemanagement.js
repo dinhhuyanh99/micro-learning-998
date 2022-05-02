83d1d12c98cf0ed284ff6b4eb1f1b36bbda3aa38
Johanna Jayme <jj969@uowmail.edu.au>
2022-05-02 21:13:27 +1000

    adding comments to the pages

--- js/coursemanagement.js
+++ js/coursemanagement.js
@@ -3,5 +3,11 @@ angular.module("mlaasMain").controller("dropdownController", function($scope){ /
         var dropdowns = document.querySelectorAll('.dropdown-trigger');
         var instances = M.Dropdown.init(dropdowns,{alignment: 'center', hover: true, coverTrigger: false}); //for options it's like JSON format https://www.w3schools.com/js/js_json_objects.asp
     };
+});
 
+//sort controller for table sorting similar to this https://codepen.io/marcmatias/pen/gxPzvY
+angular.module("mlaasMain").controller("sortController", function($scope){ 
+    $scope.sortTable = function(){
+       
+    };
 });
